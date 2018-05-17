23110b373c0deb5cde89ed572411270897c6ccea
wenllness <wenllness@DESKTOP-1B4A1LU>
2018-05-17 19:45:49 +0800

    ddddd

--- jpatterns/src/com/surge/p01/MainTest.java
+++ jpatterns/src/com/surge/p01/MainTest.java
@@ -1,9 +1,24 @@
 package com.surge.p01;
 
-public class MainTest {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	public static void main(String[] args) {
+public class MainTest {
+	// 在任何需要记录日志的类中
+		Logger log=LoggerFactory.getLogger(MainTest.class);
 		
-	}
+		public void one() {
+			log.info("into one method");
+			try {
+				System.out.println(9 / 0);
+			} catch (RuntimeException e) {
+				log.error(e.getMessage());
+			}
+			log.info("out one method");
+		}
+
+		public static void main(String[] args) {
+			new MainTest().one();
+		}
 
 }
--- patters/src/main/java/com/surge/patters/MainTest.java
+++ patters/src/main/java/com/surge/patters/MainTest.java
@@ -1,22 +1,23 @@
 package com.surge.patters;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MainTest {
 	// 在任何需要记录日志的类中
-	LoggerFactory
+	Logger log=LoggerFactory.getLogger(MainTest.class);
 	
-//	public void one() {
-//		log.info("into one method");
-//		try {
-//			System.out.println(9 / 0);
-//		} catch (RuntimeException e) {
-//			log.error(e.getMessage());
-//		}
-//		log.info("out one method");
-//	}
-//
-//	public static void main(String[] args) {
-//		new LogTest().one();
-//	}
+	public void one() {
+		log.debug("into one method");
+		try {
+			System.out.println(9 / 0);
+		} catch (RuntimeException e) {
+			log.error(e.getMessage());
+		}
+		log.info("out one method");
+	}
+
+	public static void main(String[] args) {
+		new MainTest().one();
+	}
 }
