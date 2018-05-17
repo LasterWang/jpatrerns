86ce3749327e18029a585bfc3608aa987fb6bf68
wenllness <wenllness@DESKTOP-1B4A1LU>
2018-05-17 17:05:56 +0800

    ddd

--- patters/src/main/java/com/surge/patters/LogTest.java
+++ patters/src/main/java/com/surge/patters/LogTest.java
@@ -1,5 +1,22 @@
 package com.surge.patters;
 
-public class LogTest {
+import org.slf4j.LoggerFactory;
 
+public class LogTest {
+	// 在任何需要记录日志的类中
+	LoggerFactory.getLogger();
+	
+//	public void one() {
+//		log.info("into one method");
+//		try {
+//			System.out.println(9 / 0);
+//		} catch (RuntimeException e) {
+//			log.error(e.getMessage());
+//		}
+//		log.info("out one method");
+//	}
+//
+//	public static void main(String[] args) {
+//		new LogTest().one();
+//	}
 }
